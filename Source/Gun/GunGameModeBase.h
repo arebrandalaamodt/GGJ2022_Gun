4fa9d60ca8e49819d15cf94a73f403ea8c3505a2
arebrandalaamodt <arebrandalaamodt@gmail.com>
2022-01-28 21:58:14 +0100

    First

---
+++ Source/Gun/GunGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GunGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GUN_API AGunGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
